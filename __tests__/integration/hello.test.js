0dad931a867b8e8ceaeb420b5acc3f888f758dcf
Ivo Batistela <irbatistela@gmail.com>
2020-07-20 08:19:36 -0300

    #3 Simplify Node.js first release (#4)
    * #3 Simplify Node.js first release
    
    * Refactor API
    
    * Remove husky commit verifications

--- __tests__/integration/hello.test.js
+++ __tests__/integration/hello.test.js
@@ -1,16 +1,23 @@
 const request = require('supertest');
-const faker = require('faker');
 
 const app = require('../../src/app');
 
 describe('Authentication', () => {
   it('should greet somebody', async () => {
-    const somebody = faker.name.findName();
-    const expectedName = `Hello, ${somebody}`;
     const response = await request(app)
-      .get(`/hello?name=${somebody}`)
+      .post(`/greet/Chico`)
       .send();
-    expect(response.body.message).toBe(expectedName);
+
+    expect(response.body.message).toBe('Hello, Chico!');
+    expect(response.ok).toBe(true);
+  });
+
+  it('should ask how somebody is when looking sad', async () => {
+    const response = await request(app)
+      .post(`/greet/Ivo?looking=sad`)
+      .send();
+
+    expect(response.body.message).toBe('Hello, Ivo! Is everything alright?');
     expect(response.ok).toBe(true);
   });
 });
